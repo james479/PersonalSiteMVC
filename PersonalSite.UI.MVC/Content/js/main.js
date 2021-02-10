cc1ff556e83eaf692e760a04ff7005a34413a21f
James Glover <james.e.glover1@gmail.com>
2021-02-09 20:50:38 -0600

    functional contact form

--- PersonalSite.UI.MVC/Content/js/main.js
+++ PersonalSite.UI.MVC/Content/js/main.js
@@ -35,5 +35,29 @@ $('.carousel .carousel-item').each(function () {
     }
 });
 
+
+
+// Closes responsive menu when a scroll trigger link is clicked
+$(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
+});
+
+$("body").scrollspy({
+    target: "#navbarSupportedContent",
+    offset: 74,
+});
+
+var navbarCollapse = function () {
+    if ($("#navbarSupportedContent").offset().top > 100) {
+        $("#navbarSupportedContent").addClass("navbar-shrink");
+    } else {
+        $("#navbarSupportedContent").removeClass("navbar-shrink");
+    }
+};
+// Collapse now if page is not at top
+navbarCollapse();
+// Collapse the navbar when page is scrolled
+$(window).scroll(navbarCollapse);
+
 //carousel
 
--- PersonalSite.UI.MVC/Controllers/HomeController.cs
+++ PersonalSite.UI.MVC/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using PersonalSite.UI.MVC.Models;
 
 namespace PersonalSite.UI.MVC.Controllers
 {
@@ -13,27 +16,35 @@ namespace PersonalSite.UI.MVC.Controllers
             return View();
         }
 
-        public ActionResult Resume()
+        [HttpPost]
+        public JsonResult ContactAjax(ContactViewModel cvm)
         {
-            return View();
+            string body = $"{cvm.Name} has sent you the following message: <br/>{cvm.Message} <strong> from the email address:</strong> {cvm.Email}";
 
+            MailMessage m = new MailMessage("Admin@codingbyjames.com", "james.e.glover57@gmail.com", cvm.Subject, body);
+
+            m.IsBodyHtml = true;
+
+            m.Priority = MailPriority.High;
+
+            m.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient("mail.codingbyjames.com");
+
+            client.Credentials = new NetworkCredential("Admin@codingbyjames.com", "*James*1979");
+
+            client.Port = 8889;
+            try
+            {
+                client.Send(m);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.message = $"We're sorry your request cannot be sent at this time. Please try again later. <br/>Error Message: <br/>{ex.StackTrace}";
+            }
+
+            return Json(cvm);
         }
-
-        public ActionResult Projects()
-        {
-            return View();
-        }
-
-        public ActionResult Links()
-        {
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            return View();
-        }
-
         
     }
 }
